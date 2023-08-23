bf3bcad9d781b44b4a497d9c5c7ebbfde7dfcbc6
ayush <ayush@example.com>
2023-08-23 13:19:59 +0530

    sv

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.getElementById("blockButton").addEventListener("click", () => {
+    // Use browser API to block YouTube (you need to implement this)
+    
+  });
+  
+  document.getElementById("unblockButton").addEventListener("click", () => {
+    // Use browser API to unblock YouTube (you need to implement this)
+  });
+  
